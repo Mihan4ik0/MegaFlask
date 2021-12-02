a41e676231ddf7926a0146da7d78260754d56fda
Mihan4ik0 <miha23112003@gmail.com>
2021-12-02 10:49:57 +0300

    add follow

--- app/models.py
+++ app/models.py
@@ -6,6 +6,12 @@ from app import login
 from hashlib import md5
 
 
+followers = db.Table('followers',  # Вспомогательная таблица
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                     )
+
+
 class User(UserMixin, db.Model):  # таблица пользователей в БД
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -14,6 +20,12 @@ class User(UserMixin, db.Model):  # таблица пользователей в
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,  # secondary - таблица конфигураций
+        primaryjoin=(followers.c.follower_id == id),  # условие связывающее с левой стороны(follower)
+        secondaryjoin=(followers.c.followed_id == id),  # условие связывающее с правой стороны(followed)
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'  # backref определяет доступность с правой стороны, lazy определяет режим выполнения запроса
+    )
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -27,6 +39,24 @@ class User(UserMixin, db.Model):  # таблица пользователей в
     def avatar(self, size):  # изображение аватара
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()   # конвертирование почты в нижний регистр, далее в 16-ю строку
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+    def follow(self, user):  # метод подписки
+        if not self.is_following(user):  # проверка на отсутствие подписки
+            self.followed.append(user)  # подписка на пользователя
+
+    def unfollow(self, user):  # метод отписки
+        if self.is_following(user):  # проверка на наличие подписка
+            self.followed.remove(user)  # удаление подписки
+
+    def is_following(self, user):  # метод проверки на наличие подписки
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):  # единый запрос БД
+        followed = Post.query.join(  # объединение в таблицу ассоциаций подписчиков
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)  # фильтрация, чтобы вывести только тех за кем следит пользователь
+        own = Post.query.filter_by(user_id=self.id)  # вывод своих постов
+        return followed.union(own).order_by(Post.timestamp.desc())  # объединение 'followed' и 'own', а после сортировка по времени публикации
 
 
 class Post(db.Model):  # таблица постов в БД
